6b12c3f919a3e165feec89cf9dcebd070819ebd0
rabbaniST <golamrabbanig5@gmail.com>
2024-02-14 00:03:26 +0600

    Admin Middleware and modle create

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
 
-class Authenticate extends Middleware
+class Admin extends Middleware
 {
 
     protected function redirectTo(Request $request): ?string
