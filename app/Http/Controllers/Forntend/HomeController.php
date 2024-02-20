0543a397ff16db5adcc8c3670f4f927ef54e4c88
rabbaniST <golamrabbanig5@gmail.com>
2024-02-20 09:38:57 +0600

    Home page data show and error fix

--- app/Http/Controllers/Forntend/HomeController.php
+++ app/Http/Controllers/Forntend/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Forntend;
 
+use App\Models\HomePageItem;
 use App\Http\Controllers\Controller;
 
 
@@ -9,6 +10,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return view('Forntend.home');
+        $home_page_data = HomePageItem::where('id',1)->first();
+        return view('Forntend.home',compact('home_page_data'));
     }
 }
--- resources/views/Forntend/home.blade.php
+++ resources/views/Forntend/home.blade.php
@@ -2,18 +2,16 @@
 @extends('Forntend.layout.app')
 
 @section('main-content')
-    <div class="slider" style="background-image: url({{asset('forntend/uploads/banner5.jpg') }})">
+    <div class="slider" style="background-image: url({{asset('forntend/uploads/'.$home_page_data->background)}})">
         <div class="bg"></div>
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
                     <div class="item">
                         <div class="text">
-                            <h2>Find Your Desired Job</h2>
+                            <h2>{{$home_page_data->heading}}</h2>
                             <p>
-                                Search the best, perfect and suitable jobs
-                                that matches your skills in your expertise
-                                area.
+                                {!!$home_page_data->text!!}
                             </p>
                         </div>
                         <div class="search-section">
@@ -23,14 +21,14 @@
                                         <div class="col-lg-3">
                                             <div class="form-group">
                                                 <input type="text" name="" class="form-control"
-                                                    placeholder="Job Title" />
+                                                    placeholder="{{$home_page_data->job_title}}" />
                                             </div>
                                         </div>
                                         <div class="col-lg-3">
                                             <div class="form-group">
                                                 <select name="" class="form-select select2">
                                                     <option value="">
-                                                        Job Location
+                                                        {{$home_page_data->job_location}}
                                                     </option>
                                                     <option value="">
                                                         Australia
@@ -60,7 +58,7 @@
                                             <div class="form-group">
                                                 <select name="" class="form-select select2">
                                                     <option value="">
-                                                        Job Category
+                                                        {{$home_page_data->job_category}}
                                                     </option>
                                                     <option value="">
                                                         Accounting
@@ -80,7 +78,7 @@
                                         <div class="col-lg-3">
                                             <button type="submit" class="btn btn-primary">
                                                 <i class="fas fa-search"></i>
-                                                Search
+                                                {{$home_page_data->search}}
                                             </button>
                                         </div>
                                     </div>
@@ -558,7 +556,7 @@
                 <div class="col-lg-4 col-md-6">
                     <div class="item">
                         <div class="photo">
-                            <img src="{{ asset('forntend/uploads/banner1.jpg') }}" alt="" />
+                            <img src="{{ asset('forntend/uploads/'.$home_page_data->background) }}" alt="" />
                         </div>
                         <div class="text">
                             <h2>
